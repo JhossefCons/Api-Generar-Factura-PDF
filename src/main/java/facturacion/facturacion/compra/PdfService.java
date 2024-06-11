f1e0ec1a56403cb8fa517022512cad16bb0cfd27
JhossefCons <jhniconstain@unicauca.edu.co>
2024-06-11 10:46:42 -0500

    Avances en formato

--- src/main/java/facturacion/facturacion/compra/PdfService.java
+++ src/main/java/facturacion/facturacion/compra/PdfService.java
@@ -2,17 +2,16 @@ package facturacion.facturacion.compra;
 
 import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
-import com.itextpdf.kernel.color.*;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.border.Border;
 import com.itextpdf.layout.element.*;
-import com.itextpdf.layout.property.HorizontalAlignment;
 import com.itextpdf.layout.property.TextAlignment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import javax.swing.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -22,11 +21,22 @@ import java.util.List;
 @Service
 public class PdfService {
 
+    // Funcion que recibe 2 String para hacer un parrafo con uno de ellos en negrita
+    public Paragraph boldText(String textToBold, String textToNormal){
+        Paragraph paragraph = new Paragraph();
+        Text textbold = new Text(textToBold).setBold();
+        Text textNormal = new Text(textToNormal);
+        paragraph.add(textbold);
+        paragraph.add(textNormal);
+        return paragraph;
+    }
+
     public byte[] generateInvoicePdf() throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PdfWriter writer = new PdfWriter(baos);
         PdfDocument pdfDoc = new PdfDocument(writer);
         Document document = new Document(pdfDoc);
+        document.setFontSize(10);
 
         Image logo = null;
         try {
@@ -44,85 +54,56 @@ public class PdfService {
         }
         document.add(new Paragraph("Nombre de la Empresa")
                 .setTextAlignment(TextAlignment.CENTER).setBold());
-        document.add(new Paragraph("Dirección: XXXXX")
-                .setTextAlignment(TextAlignment.CENTER));
-        document.add(new Paragraph("NIT: XXXXXXX")
-                .setTextAlignment(TextAlignment.CENTER));
-        document.add(new Paragraph("Contacto: XXXXX")
-                .setTextAlignment(TextAlignment.CENTER));
+        document.add(boldText("Direccion: ","**Direccion**").setTextAlignment(TextAlignment.CENTER));
+        document.add(boldText("NIT: ","***NIT***").setTextAlignment(TextAlignment.CENTER));
+        document.add(boldText("Contacto: ","**Contacto**").setTextAlignment(TextAlignment.CENTER));
+        document.add(new Paragraph("Factura Electrónica de compra").setTextAlignment(TextAlignment.CENTER).setBold().setFontSize(14));
+        document.add(new Paragraph("No. POBE 505").setTextAlignment(TextAlignment.CENTER));
+        document.add(new Paragraph("Resolución de facturación aprobado por la DIAN No. 18764063496317").setTextAlignment(TextAlignment.CENTER).setBold());
 
-        document.add(new Paragraph("Factura Electrónica de Venta")
-                .setTextAlignment(TextAlignment.CENTER).setBold().setFontSize(14));
-        document.add(new Paragraph("No. POBE 505")
-                .setTextAlignment(TextAlignment.CENTER));
-        document.add(new Paragraph("Resolución de facturación aprobado por la DIAN No. 18764063496317")
-                .setTextAlignment(TextAlignment.CENTER));
-
-        // detalles de factura
+        // Tabla detalles de factura
         Table invoiceDetails = new Table(2);
         invoiceDetails.setWidthPercent(100);
-        invoiceDetails.addCell(new Cell().add(new Paragraph("FECHA DE EMISIÓN: 2024/05/06").setBold())
-                .setBorder(Border.NO_BORDER));
-        invoiceDetails.addCell(new Cell().add(new Paragraph("AUT. NUMERACIÓN FAC: 18764063496317").setBold())
-                .setBorder(Border.NO_BORDER));
-        invoiceDetails.addCell(new Cell().add(new Paragraph("HORA DE EMISIÓN: 17:04:59").setBold())
-                .setBorder(Border.NO_BORDER));
-        invoiceDetails.addCell(new Cell().add(new Paragraph("PREFIJO: POBE").setBold())
-                .setBorder(Border.NO_BORDER));
-        invoiceDetails.addCell(new Cell().add(new Paragraph("FECHA DE VENCIMIENTO: 2024/05/06").setBold())
-                .setBorder(Border.NO_BORDER));
-        invoiceDetails.addCell(new Cell().add(new Paragraph("RANGO AUTORIZADO: 1-10000").setBold())
-                .setBorder(Border.NO_BORDER));
-        invoiceDetails.addCell(new Cell().add(new Paragraph("MÉTODO DE PAGO: Contado").setBold())
-                .setBorder(Border.NO_BORDER));
-        invoiceDetails.addCell(new Cell().add(new Paragraph("FECHA INICIO DE VIGENCIA: 2024/01/09").setBold())
-                .setBorder(Border.NO_BORDER));
+        invoiceDetails.addCell(new Cell().add(boldText("FECHA DE EMISIÓN:"," 2024/05/06")));
+        invoiceDetails.addCell(new Cell().add(boldText("AUT. NUMERACIÓN FAC:"," 18764063496317")));
+        invoiceDetails.addCell(new Cell().add(boldText("HORA DE EMISIÓN:"," 17:04:59")));
+        invoiceDetails.addCell(new Cell().add(boldText("PREFIJO:"," POBE")));
+        invoiceDetails.addCell(new Cell().add(boldText("FECHA DE VENCIMIENTO:"," 2024/05/06")));
+        invoiceDetails.addCell(new Cell().add(boldText("RANGO AUTORIZADO:"," 1-10000")));
+        invoiceDetails.addCell(new Cell().add(boldText("MÉTODO DE PAGO:"," Contado")));
+        invoiceDetails.addCell(new Cell().add(boldText("FECHA INICIO DE VIGENCIA:"," 2024/01/09")));
         document.add(invoiceDetails);
 
-        // detalles del cliente
-        document.add(new Paragraph("DATOS DEL RECEPTOR")
-                .setTextAlignment(TextAlignment.CENTER).setBold());
+        // Tabla detalles del tercero
+        document.add(new Paragraph("DATOS DEL TERCERO").setTextAlignment(TextAlignment.CENTER).setBold());
         Table clientDetails = new Table(2);
         clientDetails.setWidthPercent(100);
-        clientDetails.addCell(new Cell().add(new Paragraph("CÓDIGO DEL CLIENTE: XXXXX").setBold())
-                .setBorder(Border.NO_BORDER));
-        clientDetails.addCell(new Cell().add(new Paragraph("DEPARTAMENTO: Cauca").setBold())
-                .setBorder(Border.NO_BORDER));
-        clientDetails.addCell(new Cell().add(new Paragraph("TIPO DE DOCUMENTO: Cédula de ciudadanía").setBold())
-                .setBorder(Border.NO_BORDER));
-        clientDetails.addCell(new Cell().add(new Paragraph("CIUDAD: POPAYÁN").setBold())
-                .setBorder(Border.NO_BORDER));
-        clientDetails.addCell(new Cell().add(new Paragraph("NÚMERO DE DOCUMENTO: XXXXX").setBold())
-                .setBorder(Border.NO_BORDER));
-        clientDetails.addCell(new Cell().add(new Paragraph("TELEFONO: +57311...").setBold())
-                .setBorder(Border.NO_BORDER));
-        clientDetails.addCell(new Cell().add(new Paragraph("NOMBRE DE CLIENTE: XXXXX").setBold())
-                .setBorder(Border.NO_BORDER));
-        clientDetails.addCell(new Cell().add(new Paragraph("CORREO: @gmail.com").setBold())
-                .setBorder(Border.NO_BORDER));
-        clientDetails.addCell(new Cell().add(new Paragraph("DIRECCIÓN DEL CLIENTE: XXXXX").setBold())
-                .setBorder(Border.NO_BORDER));
-        clientDetails.addCell(new Cell().add(new Paragraph("TIPO DE PERSONA: Persona Natural").setBold())
-                .setBorder(Border.NO_BORDER));
+        clientDetails.addCell(new Cell().add(boldText("CÓDIGO DEL CLIENTE:"," **codigo**")));
+        clientDetails.addCell(new Cell().add(boldText("DEPARTAMENTO:","**departamento***")));
+        clientDetails.addCell(new Cell().add(boldText("TIPO DE DOCUMENTO:","**Cédula**")));
+        clientDetails.addCell(new Cell().add(boldText("CIUDAD:","**ciudad**")));
+        clientDetails.addCell(new Cell().add(boldText("NÚMERO DE DOCUMENTO:","**documento**")));
+        clientDetails.addCell(new Cell().add(boldText("TELEFONO:","**+57311**")));
+        clientDetails.addCell(new Cell().add(boldText("NOMBRE DE CLIENTE:","**nombre**")));
+        clientDetails.addCell(new Cell().add(boldText("CORREO:","**@gmail.com**")));
+        clientDetails.addCell(new Cell().add(boldText("DIRECCIÓN DEL CLIENTE:","**direccion cliente**")));
+        clientDetails.addCell(new Cell().add(boldText("TIPO DE PERSONA:","**tipo de persona**")));
         document.add(clientDetails);
 
         // Tabla de productos
-        document.add(new Paragraph("ÍTEM").setBold());
+        document.add(new Paragraph("PRODUCTOS").setTextAlignment(TextAlignment.CENTER).setBold());
         Table itemTable = new Table(new float[]{2, 2, 2, 2, 2, 2, 2});
         itemTable.setWidthPercent(100);
-        itemTable.addHeaderCell(new Cell().add(new Paragraph("CÓDIGO DE PRODUCTO").setBold()));
-        itemTable.addHeaderCell(new Cell().add(new Paragraph("DESCRIPCIÓN").setBold()));
-        itemTable.addHeaderCell(new Cell().add(new Paragraph("UM").setBold()));
-        itemTable.addHeaderCell(new Cell().add(new Paragraph("CANTIDAD").setBold()));
-        itemTable.addHeaderCell(new Cell().add(new Paragraph("VALOR UNITARIO").setBold()));
-        itemTable.addHeaderCell(new Cell().add(new Paragraph("DESCUENTO").setBold()));
-        itemTable.addHeaderCell(new Cell().add(new Paragraph("VALOR IMPUESTO").setBold()));
-        itemTable.addHeaderCell(new Cell().add(new Paragraph("VALOR TOTAL").setBold()));
+        itemTable.addHeaderCell(new Cell().add(new Paragraph("CANTIDAD").setBold().setFontSize(8)));
+        itemTable.addHeaderCell(new Cell().add(new Paragraph("DESCRIPCIÓN").setBold().setFontSize(8)));
+        itemTable.addHeaderCell(new Cell().add(new Paragraph("IVA").setBold().setFontSize(8)));
+        itemTable.addHeaderCell(new Cell().add(new Paragraph("PRECIO UNITARIO").setBold().setFontSize(8)));
+        itemTable.addHeaderCell(new Cell().add(new Paragraph("VALOR TOTAL").setBold().setFontSize(8)));
 
         // productos
         List<List<String>> items = Arrays.asList(
-                Arrays.asList("753759280888", "VENU 2 PLUS POWDER GRAY PASSIV", "NA*S/L*6S0022517 94", "1.00", "1,989,900.00", "198,990.00", "0.00", "1,790,910.00"),
-                Arrays.asList("flete", "FLETE", "94", "1.00", "10,900.00", "0.00", "0.00", "10,900.00")
+                Arrays.asList("564", "PASTILLA DE CUAJO", "19%", "2326", "1,311,864"),
+                Arrays.asList("2", "SAL", "NO APLICA", "23,260", "46,520")
         );
 
         for (List<String> item : items) {
